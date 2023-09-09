eeb6c82b208271833dfbacce344f8b9497743d9f
OmarMWarraich <earthling986@gmail.com>
2023-09-09 17:47:07 +0500

    feat: Window Event

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,5 +1,15 @@
+import { useEffect } from "react"
+
 export default function WindowEvent () {
+    useEffect(() => {
+        const doubleClick = () => alert('double click')
+
+        window.addEventListener('dblclick', doubleClick)
+
+        return () => 
+            window.removeEventListener('dblclick', doubleClick)
+    }, [])
     return (
-      <h2>Window event active</h2>
+      <h2>Window event is active</h2>
     )
   }
